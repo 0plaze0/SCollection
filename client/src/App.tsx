d9ee567f94caede2918a920170ccc89734dea923
plaze <banikakash131@gmail.com>
2024-07-01 16:54:09 +0530

    feat(components): add Header and Layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Register } from "./pages";
+import { Layout } from "./components";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<h1>Hello</h1>} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
