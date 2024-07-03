7737fb119620c4996d20932cb634a3989b0925ec
plaze <banikakash131@gmail.com>
2024-07-03 20:45:05 +0530

    feat: add userRegistration schema for from data validation

---
+++ api/src/schemas/userSchemas.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const userRegistrationSchema = z.object({
+  name: z
+    .string()
+    .min(5, { message: "User name length should be greater then 4" }),
+  address: z.string(),
+  phone: z.string(),
+  email: z.string().email(),
+  password: z.string().min(5),
+});
