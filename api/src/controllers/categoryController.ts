4baa92209b1b416d62d3666b0366398079efaa24
plaze <banikakash131@gmail.com>
2024-07-25 22:49:53 +0530

    feat: add update , get and delete controller

--- api/src/controllers/categoryController.ts
+++ api/src/controllers/categoryController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { categoryModel } from "../model/category";
+
 import BadRequestError from "../errors/BadRequestError";
 import slugify from "slugify";
 
@@ -7,7 +8,11 @@ const createCategory = async (req: Request, res: Response) => {
   const { name } = req.body;
   if (!name)
     throw new BadRequestError({ code: 400, message: "Please provide a name" });
-
+  const duplicate = await categoryModel.find({ name });
+  if (duplicate)
+    return res
+      .status(200)
+      .json({ success: true, message: "Category Already exist" });
   const category = await categoryModel.create({
     name,
     slug: slugify(name),
@@ -18,4 +23,78 @@ const createCategory = async (req: Request, res: Response) => {
     .json({ succes: true, message: "Successfully create category" });
 };
 
-export { createCategory };
+const getCategory = async (req: Request, res: Response) => {
+  const { slug } = req.params;
+  if (!slug)
+    throw new BadRequestError({
+      code: 400,
+      message: "Please Provide valid name",
+    });
+  console.log(slug);
+  const category = await categoryModel.findOne({ slug });
+  console.log(category);
+  if (!category)
+    throw new BadRequestError({ code: 404, message: "Category not found" });
+
+  return res.status(200).send({ success: true, category });
+};
+
+const getAllCategory = async (req: Request, res: Response) => {
+  const category = await categoryModel.find({});
+
+  return res.status(200).send({ success: true, category });
+};
+const updateCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!id)
+    throw new BadRequestError({
+      code: 400,
+      message: "Please Provide valid id",
+    });
+  if (!name)
+    throw new BadRequestError({
+      code: 400,
+      message: "Please Provide valid category name",
+    });
+
+  const ifExist = await categoryModel.findById(id);
+  if (!ifExist)
+    throw new BadRequestError({ code: 404, message: "Category doesn't exist" });
+
+  const updateCategory = await categoryModel.findByIdAndUpdate(
+    id,
+    { name, slug: slugify(name) },
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json({ success: true, message: "Category Updated", updateCategory });
+};
+const deleteCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  if (!id)
+    throw new BadRequestError({
+      code: 400,
+      message: "Please Provide valid id",
+    });
+  const ifExist = await categoryModel.findById(id);
+  if (!ifExist)
+    throw new BadRequestError({ code: 404, message: "Category doesn't exist" });
+
+  const deleteCategory = await categoryModel.findByIdAndDelete(id);
+
+  return res
+    .status(200)
+    .send({ success: true, message: "Category Deleted Successfully" });
+};
+
+export {
+  createCategory,
+  getCategory,
+  getAllCategory,
+  updateCategory,
+  deleteCategory,
+};
--- api/src/routes/category.ts
+++ api/src/routes/category.ts
@@ -2,7 +2,13 @@ import express from "express";
 import { dataValidation } from "../middleware/validation";
 import { isAdmin, isLoggedIn } from "../middleware/authMiddleware";
 import { categorySchema } from "../schemas/categorySchema";
-import { createCategory } from "./../controllers/categoryController";
+import {
+  createCategory,
+  deleteCategory,
+  getAllCategory,
+  getCategory,
+  updateCategory,
+} from "./../controllers/categoryController";
 
 const router = express.Router();
 
@@ -13,5 +19,9 @@ router.post(
   dataValidation(categorySchema),
   createCategory
 );
+router.get("/get-category", isLoggedIn, isAdmin, getAllCategory);
+router.get("/get-category/:slug", isLoggedIn, isAdmin, getCategory);
+router.put("/update-category/:id", isLoggedIn, isAdmin, updateCategory);
+router.delete("/delete-category/:id", isLoggedIn, isAdmin, deleteCategory);
 
 export default router;
